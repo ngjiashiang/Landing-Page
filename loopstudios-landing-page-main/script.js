86c567cadd9eca4bd0afacc5e0d7a95abb2e3a54
ngjiashiang <73417501+ngjiashiang@users.noreply.github.com>
2021-03-08 02:29:03 +0800

    Add files via upload

---
+++ loopstudios-landing-page-main/script.js
@@ -0,0 +1,6 @@
+function menuExtend(){
+      document.querySelector(".navbar").style.display = "contents";
+    }
+function menuCollapse() {
+    document.querySelector(".navbar").style.display = "none";
+}
